6c627492c057d7eaec179824b2b3a543155aa2aa
Erick Lincoln <ericklincoln@live.com>
2022-09-11 20:14:58 -0300

    Código rodando com moedas pré-definidas

--- main.py
+++ main.py
@@ -10,4 +10,13 @@ dados = resposta.json()
 # print(dados.values()) #retorna uma lista de todos os valores do dicionario
 # print(dados.items()) #retorna uma lista de tuplas (chave, valor) do dicionario
 valoresdemoedas = dados['rates'] #dicionário das cotações criado
-print(valoresdemoedas)
+real = valoresdemoedas.get('BRL')
+dolar = valoresdemoedas.get('USD')
+print('Olá!\n Seja bem vindo ao contador de moedas!\n')
+print('R${:.2f} pra dolar U$${:.2f}'.format(real, dolar))
+
+#manipulaçao de dicionário e conversor simples de Real para dolar criado
+#side project LestCode e Santander
+valorDeConversao = float(input('informe o valor a ser convertido:\nR$ '))
+cotaçãoDoValor = valoresdemoedas['BRL']
+print(f'R${valorDeConversao} pra dolar US${valorDeConversao/cotaçãoDoValor:.2f}')
